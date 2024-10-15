0280e94cfe7ed0588837b65ae87ae8b9df55a37c
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2024-10-15 12:21:07 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,8 @@
+const numbers=[34,23,21,35,67,87,34];
+console.log(numbers.length);
+numbers.push(12);
+console.log(numbers.length);
+
+// console.log(numbers.index[1]);
+
+console.log(numbers.indexOf(67,4));
---
+++ condition.js
@@ -0,0 +1,7 @@
+const x=5;
+if(!x)
+    console.log('x is true');
+else
+console.log('x is false');
+
+console.log(x?'x is true' : 'x is false');
